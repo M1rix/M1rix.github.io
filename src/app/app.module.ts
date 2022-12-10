8da07c2d1b0d3e11e2a081dcaa84555c933e3c1d
Mirix <mir.allaberganov@gmail.com>
2022-12-10 12:15:27 +0500

    make some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './layout/main/home/home.component';
+import { AboutComponent } from './layout/main/about/about.component';
+import { ProjectsComponent } from './layout/main/projects/projects.component';
+import { ContactComponent } from './layout/main/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
