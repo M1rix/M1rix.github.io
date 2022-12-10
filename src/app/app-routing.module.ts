20a5014990e9fcc67376f005bb1b2539aef015ba
Mirix <mir.allaberganov@gmail.com>
2022-12-10 12:15:27 +0500

    add bootstrap and layout components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { navbarRoute } from "./layout/navbar/navbar.route";
+import { footerRoute } from "./layout/footer/footer.route";
+import { mainRoute } from "./layout/main/main.route";
 
-const routes: Routes = [];
+const routes: Routes = [navbarRoute,mainRoute, footerRoute];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
