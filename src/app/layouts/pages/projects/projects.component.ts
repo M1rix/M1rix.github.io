b175be67b147b0fb8d0740dd14fba18ee3680b72
M1rix <mir.allaberganov@gmail.com>
2025-02-07 22:38:38 +0500

    Customize projects page

--- src/app/layouts/pages/projects/projects.component.ts
+++ src/app/layouts/pages/projects/projects.component.ts
@@ -1,17 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ThreeSceneComponent } from '../../hero/three-scene/three-scene.component';
-import { NgOptimizedImage } from "@angular/common";
+import { CommonModule, NgOptimizedImage } from "@angular/common";
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [
     ThreeSceneComponent,
-    NgOptimizedImage
+    CommonModule
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
 
+  projects = [
+    {
+      title: 'adviceslips',
+      description: 'Short info about project one.',
+      details: 'Detailed information about this project.',
+      image: 'https://raw.githubusercontent.com/M1rix/image-store/refs/heads/master/previews/adviceslips.png'
+    },
+    {
+      title: 'pomodorix',
+      description: 'Short info about project two.',
+      details: 'Detailed information about this project.',
+      image: 'https://raw.githubusercontent.com/M1rix/image-store/refs/heads/master/previews/pomodorix.png'
+    },
+    {
+      title: 'weatherix',
+      description: 'Short info about project three.',
+      details: 'Detailed information about this project.',
+      image: 'https://raw.githubusercontent.com/M1rix/image-store/refs/heads/master/previews/weatherix.png'
+    },
+  ];
+
+  activeIndex = 0;
+
+  @HostListener('wheel', ['$event'])
+  onScroll(event: WheelEvent) {
+    if (event.deltaY > 0) {
+      if (this.activeIndex < this.projects.length - 1) {
+        this.activeIndex++;
+      }
+    } else {
+      if (this.activeIndex > 0) {
+        this.activeIndex--;
+      }
+    }
+  }
+
+  setActiveIndex(index: number) {
+    if (index < 0 || index >= this.projects.length) return;
+
+    gsap.to(this, {
+      duration: 1,
+      activeIndex: index,
+      ease: 'power2.out',
+      onUpdate: () => {
+        this.activeIndex = Math.round(this.activeIndex);
+      },
+    });
+  }
 }
